a8cef30096da574459472281f477f813eed9f463
FIRST_NAME LAST_NAME <aarav.sethi@outlook.com>
2023-10-20 20:30:01 -0500

    Improved XML

--- texteng.py
+++ texteng.py
@@ -7,7 +7,11 @@ False = false;
 -- Compiled:
 return """
 import json as jslib
-import xml.etree.ElementTree as ET
+import log
+try:
+    import xmltodict
+except:
+    log.error("please install xmltodict using 'pip install xmltodict' for XML support")
 
 class TextEng:
     def json(code):
@@ -26,17 +30,20 @@ class TextEng:
         lua_str = lua_str.replace("]]", "]]..\"]]\"..[[")
         return WRAPPER + lua_str
     def xml(xml_str):
-        root = ET.fromstring(xml_str)
-
-        def convert(elem):
-            if len(elem) == 0:
-                return f'"{elem.text}"'
-            else:
-                return "{" + ", ".join(f"[{convert(child.tag)}] = {convert(child)}" for child in elem) + "}"
-
-        lua_str = convert(root)
-        lua_str = lua_str.replace("]]", "]]..\"]]\"..[[")
+        data = xmltodict.parse(xml_str)
         
+        def convert(obj):
+            if isinstance(obj, dict):
+                return "{" + ", ".join(f"{{{convert(k)}, {convert(v)}}}" for k, v in obj.items()) + "}"
+            elif isinstance(obj, list):
+                return "{" + ", ".join(convert(item) for item in obj) + "}"
+            elif isinstance(obj, str):
+                return f'"{obj}"'
+            else:
+                return str(obj)
+
+        lua_str = convert(data)
+        lua_str = lua_str.replace("]]", "]]..\"]]\"..[[")
         return WRAPPER + lua_str
     def txt(code):
         code = code.replace("]]", "]]..\"]]\"..[[")
